68ce5f14642ed2436c867ed6f751ded881963490
Tomaso Leoni <tomasoleoni011@gmail.com>
2019-09-18 23:01:35 +0200

    setting up structure for translation file

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React from "react";
+import { Trans, withNamespaces } from "react-i18next";
 
-export default class About extends React.Component {
-  render() {
+function About ({ t }) {
     return (
       <div className="card m-3">
         <div
@@ -20,7 +20,9 @@ export default class About extends React.Component {
               aria-expanded="false"
               aria-controls="collapseOne"
             >
-              More about me
+              <Trans>
+                {t("moreAboutMeTitle")}
+              </Trans>
             </button>
           </h5>
         </div>
@@ -31,29 +33,14 @@ export default class About extends React.Component {
           aria-labelledby="headingOne"
           data-parent="#accordion"
         >
-          {/* After I have finished my school for
-                          information technology in Berne, I would like to
-                          venture towards more sophisticated topics, such as
-                          artificial intelligence, medical information
-                          technologies or other areas I already mentioned above.
-                          Studying in one of these fields is also a plan of
-                          mine, although I am currently still undecided as to
-                          what program I will end up taking up: Medical
-                          Information Technology, Bioinformatics and Biomedical
-                        Engineering or just plain old Computer Science? --> */}
           <div className="card-body text-justify text-container">
-            My name is Tomaso Aurelio Domenico Leoni and I am a 16 year old
-            application developer from Switzerland, I however grew up in
-            Germany, where I spent the first 11 years of my life. I have always
-            been very invested in all sorts of technologies, so I always knew I
-            was predestined to be working in this market branch one day. During
-            my free time, I love working on my own personal projects or help
-            others out with their projects. Apart from technology and software
-            development though, I am also a great fan of learning languages,
-            motor sports, fashion, music and aesthetics.
+            <Trans>
+              {t("moreAboutMeText")}
+            </Trans>
           </div>
         </div>
       </div>
     );
-  }
 }
+
+export default withNamespaces()(About);
--- src/i18n.js
+++ src/i18n.js
@@ -13,34 +13,140 @@ i18n
         translations: {
           title: "Hi! My name's Tomaso",
           leadingText:
-            "I am an Application developer from Switzerland, with a current major interest in technologies of the future such as artificial intelligence, machine learning, medical informatics and biomechanical engineering. Currently, I am in my last year at the School for Information Technology of Berne, about to head into a one year internship"
+            "I am an Application developer from Switzerland, with a current major interest in technologies of the future such as artificial intelligence, machine learning, medical informatics and biomechanical engineering. Currently, I am in my last year at the School for Information Technology of Berne, about to head into a one year internship",
+          moreAboutMeTitle: "More about me",
+          moreAboutMeText: "My name is Tomaso Aurelio Domenico Leoni and I am a 16 year old application developer from Switzerland, I however grew up in Germany, where I spent the first 11 years of my life. I have always been very invested in all sorts of technologies, so I always knew I was predestined to be working in this market branch one day. During my free time, I love working on my own personal projects or help others out with their projects. Apart from technology and software development though, I am also a great fan of learning languages, motor sports, fashion, music and aesthetics.",
+          skillsTitle: "Skills / CV",
+          skillsInfoText: "Take a look at my complete CV or catch a glimpse of my skills below:",
+          skillsLeftTite: "Programming skills:",
+          skillsRightTitle: "Language skills:",
+          codingSkills: [
+            "• Deep knowledge of Java, C#, Dart, Python, JavaScript, Ruby, HTML & CSS",
+            "• Broad experience in mobile and web development using Flutter, Android Studio, Ruby on Rails, ReactJS & NodeJS",
+            "• Great knowledge of databases: SQLite, MySQL, Dokumentbasierte Datenbanken (MongoDB & Firebase)"
+          ],
+          german: "",
+          italian: "",
+          english: "",
+          french: "",
+          swedish: "",
+          projectsTitle: "",
+          noteableText: "",
+          moneylogText: "",
+          matchedText: "",
+          splashxText: "",
+          portfolioText: ""
         }
       },
       de: {
         translations: {
           title: "Hallo! Ich heisse Tomaso",
           leadingText:
-            "Ich bin ein Applikationsentwickler aus der Schweiz mit einem brennendem Interesse für die Technologien der Zukunft, wie zum Beispiel Künstliche Intelligenz, Maschinelles Lernen, Medizininformatik und Biomedizinische Ingenieurwissenschaften. Zurzeit besuche ich das letzte Schuljahr der IMS (Informatikmittelschule) Bern und befinde mich inmitten der Bewerbungsphase für mein nächstjähriges Praktikum"
+            "Ich bin ein Applikationsentwickler aus der Schweiz mit einem brennendem Interesse für die Technologien der Zukunft, wie zum Beispiel Künstliche Intelligenz, Maschinelles Lernen, Medizininformatik und Biomedizinische Ingenieurwissenschaften. Zurzeit besuche ich das letzte Schuljahr der IMS (Informatikmittelschule) Bern und befinde mich inmitten der Bewerbungsphase für mein nächstjähriges Praktikum",
+          moreAboutMeTitle: "Mehr über mich",
+          moreAboutMeText: "",
+          skillsTitle: "Fähigkeiten / CV",
+          skillsInfoText: "",
+          skillsLeftTite: "",
+          skillsRightTitle: "",
+          codingFirstPoint: "",
+          codingSecondPoint: "",
+          codingThirdPoint: "",
+          german: "",
+          italian: "",
+          english: "",
+          french: "",
+          swedish: "",
+          projectsTitle: "",
+          noteableText: "",
+          moneylogText: "",
+          matchedText: "",
+          splashxText: "",
+          portfolioText: ""
         }
       },
       swe: {
         translations: {
-          title: "Hey! Jag heter Tomaso"
+          title: "Hey! Jag heter Tomaso",
+          leadingText: "",
+          moreAboutMeTitle: "",
+          moreAboutMeText: "",
+          skillsTitle: "",
+          skillsInfoText: "",
+          skillsLeftTite: "",
+          skillsRightTitle: "",
+          codingFirstPoint: "",
+          codingSecondPoint: "",
+          codingThirdPoint: "",
+          german: "",
+          italian: "",
+          english: "",
+          french: "",
+          swedish: "",
+          projectsTitle: "",
+          noteableText: "",
+          moneylogText: "",
+          matchedText: "",
+          splashxText: "",
+          portfolioText: ""
         }
       },
       it: {
         translations: {
-          title: "Ciao! Mi chiamo Tomaso"
+          title: "Ciao! Mi chiamo Tomaso",
+          leadingText: "",
+          moreAboutMeTitle: "",
+          moreAboutMeText: "",
+          skillsTitle: "",
+          skillsInfoText: "",
+          skillsLeftTite: "",
+          skillsRightTitle: "",
+          codingFirstPoint: "",
+          codingSecondPoint: "",
+          codingThirdPoint: "",
+          german: "",
+          italian: "",
+          english: "",
+          french: "",
+          swedish: "",
+          projectsTitle: "",
+          noteableText: "",
+          moneylogText: "",
+          matchedText: "",
+          splashxText: "",
+          portfolioText: ""
         }
       },
       fr: {
         translations: {
-          title: "Bonjour! Je m'appelle Tomaso"
+          title: "Bonjour! Je m'appelle Tomaso",
+          leadingText: "",
+          moreAboutMeTitle: "",
+          moreAboutMeText: "",
+          skillsTitle: "",
+          skillsInfoText: "",
+          skillsLeftTite: "",
+          skillsRightTitle: "",
+          codingFirstPoint: "",
+          codingSecondPoint: "",
+          codingThirdPoint: "",
+          german: "",
+          italian: "",
+          english: "",
+          french: "",
+          swedish: "",
+          projectsTitle: "",
+          noteableText: "",
+          moneylogText: "",
+          matchedText: "",
+          splashxText: "",
+          portfolioText: ""
         }
       }
     },
     fallbackLng: "en",
     debug: true,
+    returnObjects: true,
 
     ns: ["translations"],
     defaultNS: ["translations"],
