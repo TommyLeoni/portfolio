421702c027dfe1649ad8f23e79a27a9226f14c7e
Tomaso Leoni <tomasoleoni011@gmail.com>
2019-09-22 22:30:53 +0200

    Disabled localisation until further completion

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { I18nextProvider } from "react-i18next";
 //import { Card } from './components/PopupCard';
 import App from "./App";
-import i18n from "./i18n";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <I18nextProvider i18n={i18n}>
-    <App />
-  </I18nextProvider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
