f82242b1bee398ccdcb6b168f2cece0f6229c862
Tomaso Leoni <tomasoleoni011@gmail.com>
2019-09-22 22:35:52 +0200

    Disabled localisation until further completion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap";
 import React from "react";
-import { Trans, withNamespaces } from "react-i18next";
 import "./App.css";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
-function App ({ t }) {
+export default class App extends React.Component {
+  render() {
     return (
       <div className="App">
         <div className="row justify-content-center text-center h-100">
@@ -20,9 +20,7 @@ function App ({ t }) {
           </div>
           <div className="col-sm-8 col-11 px-0 my-sm-auto mt-3 landing">
             <div className="d-inline-block">
-              <h1 className="greeting">
-                <Trans>{t("title")}</Trans>
-              </h1>
+              <h1 className="greeting">Hi! My name's Tomaso</h1>
             </div>
             <div className="leading-text">
               <div className="row justify-content-center text-center landing">
@@ -51,5 +49,4 @@ function App ({ t }) {
       </div>
     );
   }
-
-  export default withNamespaces()(App);
+}
